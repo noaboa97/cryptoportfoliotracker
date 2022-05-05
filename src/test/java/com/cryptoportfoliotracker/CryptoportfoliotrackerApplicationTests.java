69db0d028740384851f4c717ba5657f7c95911f6
Noaboa97 <noaboa78@gmail.com>
2022-05-05 15:47:04 +0200

    init

---
+++ src/test/java/com/cryptoportfoliotracker/CryptoportfoliotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoportfoliotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
